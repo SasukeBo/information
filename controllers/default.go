db8c5082440f2f9263b605db5daaacfdbcab4542
SasukeBo <809754210@qq.com>
2019-07-10 19:39:21 +0800

    测试信息

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,18 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Website"] = "设备资讯化"
+	c.Data["Name"] = "SasukeBo"
+	c.Data["Email"] = "809754210@qq.com"
+	c.Data["path"] = "static"
+
+	if v := c.GetSession("freshCount"); v == nil {
+		c.SetSession("freshCount", int(2))
+		c.Data["freshCount"] = 1
+	} else {
+		c.SetSession("freshCount", v.(int)+1)
+		c.Data["freshCount"] = v.(int)
+	}
+
 	c.TplName = "index.html"
 }
