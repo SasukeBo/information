13ad6c8c81a65f4f2ee1852b68618980d86d4396
SasukeBo <809754210@qq.com>
2019-07-11 11:31:16 +0800

    graphql/graphiql

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"github.com/SasukeBo/information/controllers"
+	"github.com/SasukeBo/information/schema"
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	beego.Router("/index", &controllers.MainController{})
+	beego.Handler("/graphql", schema.GraphqlHander, true)
 }
