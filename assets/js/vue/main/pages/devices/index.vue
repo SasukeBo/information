c17bb7200db2b591428762ef660d3c7322f216a0
SasukeBo <809754210@qq.com>
2019-09-30 16:27:13 +0800

    重构设备列表页

--- assets/js/vue/main/pages/devices/index.vue
+++ assets/js/vue/main/pages/devices/index.vue
@@ -1,4 +1,40 @@
-<template></template>
+<template>
+  <div class="device-list">
+    <div class="header-fix">
+      <div class="title">设备列表</div>
+      <div class="btns">
+        <el-button size="small" type="info" icon="el-icon-refresh"></el-button>
+        <el-button size="small" type="primary" @click="$router.push({name: 'device-new'})">创建设备</el-button>
+      </div>
+    </div>
+
+    <div class="device-filter">
+      <el-input
+        placeholder="设备类型/设备名称/地址/设备编号模糊搜索"
+        v-model="pattern"
+        size="small"
+        class="filter__search"
+      >
+        <el-button slot="append" type="primary" icon="el-icon-search" @click="search = pattern"></el-button>
+      </el-input>
+
+      <div class="filter__register">
+        <el-checkbox v-model="filter" label="只看我注册的" size="small"></el-checkbox>
+      </div>
+
+      <el-select v-model="status" placeholder="状态筛选" size="small" class="filter__status">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </div>
+
+    <div class="device-list__body"></div>
+  </div>
+</template>
 <script>
 import devicesQuery from './gql/query.devices.gql';
 
@@ -10,9 +46,9 @@ export default {
       query: devicesQuery,
       variables() {
         return {
-          pattern: this.pattern,
+          search: this.search,
           status: this.status,
-          isRegister: this.isRegister,
+          filter: this.filter,
           limit: this.limit,
           offset: this.offset
         };
@@ -23,11 +59,30 @@ export default {
   data() {
     return {
       pattern: '',
+      search: '',
       status: null,
-      isRegister: false,
+      filter: false,
       limit: 9,
       currentPage: 1,
       isExpand: false,
+      options: [
+        {
+          value: 'prod',
+          label: '生产中'
+        },
+        {
+          value: 'stop',
+          label: '停机中'
+        },
+        {
+          value: 'offline',
+          label: '离线'
+        },
+        {
+          value: null,
+          label: '全部'
+        }
+      ],
       deviceList: {
         total: 0,
         devices: []
@@ -41,4 +96,5 @@ export default {
 };
 </script>
 <style lang="scss">
+@import 'css/main/devices/index.scss';
 </style>
